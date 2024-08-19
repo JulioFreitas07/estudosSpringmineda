b8762bab5313c8ad816c451d1717bd4eb72ea1fe
JulioFreitas07 <freitas27j@gmail.com>
2024-08-19 20:07:39 -0300

    first commit

---
+++ src/main/java/springandvue/lab4mineda/Lab4minedaApplication.java
@@ -0,0 +1,13 @@
+package springandvue.lab4mineda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab4minedaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab4minedaApplication.class, args);
+	}
+
+}
